a9a74e454b4cbcbd9b43f8f41129bbe715fe61a0
Leandro Cooper <134981673+leecooper29@users.noreply.github.com>
2025-09-03 09:58:03 -0400

    project media queries

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import CollaborationPage from "./pages/collaboration";
 import HomePage from "./pages/homepage";
 import ProjectsPage from "./pages/projects";
 // using tailwind for styling (will implement later)
+// updated
 
 function App() {
   return (
